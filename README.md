8a293324e24f8b981a064c440cf33a65edc96fd8
Pranav Thombare <pranav@xioresearch.com>
2021-05-15 14:25:57 +0530

    Convert the code into streamlit app - use virtual env - update README

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # CoWIN Appointment Tracker
 
-This repository will contain the code to access all the APIs made open to the public by the government
+This repositry has the code which will run a localhost server that can hit the APIs and find appointments of the current date. 
 
 ## Instructions 
 
-...under construction...
+Run the following code block in your terminal. 
+
+[TESTED ON LINUX 20.04]
+
+```
+source venv/bin/activate 
+streamlit run main.py
+```
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 import json
 import pandas as pd 
+import datetime
 from hashlib import sha256
+import streamlit as st
 
 base_url = "https://cdn-api.co-vin.in/api"
 browser_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
@@ -72,9 +74,25 @@ def weeklyAppointmentByDistrict(district_id,date):
     appointments_df = pd.DataFrame(appointments['sessions'])
     return appointments_df
 
-transaction = genOTP('8488895597')
-print(confirmOTP(int('886674'), transaction['code'][0]))
-#print(appointmentByDistrict('151','13-05-2021'))
-#print(appointmentByPin('380015', '13-05-2021'))
-#print(getStates())
-#print(getDistrict('10'))
+
+st.title('CoWIN Appointment Tracker')
+
+#transaction = genOTP('8488895597')
+#print(confirmOTP(int('886674'), transaction['code'][0]))
+DATE = datetime.date.today().strftime('%d-%m-%Y')
+STATES = getStates()
+st.write(STATES)
+
+state_name = st.sidebar.selectbox('Select state',STATES['state_name'])
+st.write(f'{state_name} has following districts')
+
+STATE_ID = STATES.loc[STATES['state_name'] == state_name, 'state_id'].item()
+DISTRICTS = getDistrict(STATE_ID)
+
+district_name = st.sidebar.selectbox('Select District',DISTRICTS['district_name'])
+DISTRICT_ID = DISTRICTS.loc[DISTRICTS['district_name'] == district_name, 'district_id'].item()
+
+#district_number = input('Enter Disctrict number: ')
+st.write(appointmentByDistrict(DISTRICT_ID,DATE))
+st.write(appointmentByPin('380015', DATE))
+
